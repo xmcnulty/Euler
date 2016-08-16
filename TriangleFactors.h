a12bed515512a040593c31b86fc98e17e26ed607
xmcnulty <xavier.mcnulty@icloud.com>
2016-08-16 10:43:07 -0400

    File changes.

--- TriangleFactors.h
+++ TriangleFactors.h
@@ -11,11 +11,23 @@ typedef struct Tree {
 	int _size;
 } Tree;
 
+// initialize a prime factor tree
 Tree* initTree();
+
+// initialize a Node, each new node will be threaded
+Node* initNode(int val, Node* right);
+
+// clean up a tree
+void freeTree(Tree* t);
 
 // insert into tree return true/false
 int insert(Tree* t, int val);
 
+// checks if a number is prime
 int isPrime(long n);
 
+// gets the nth triangle number
 long getNthTriangleNumbers(int n);
+
+// DEBUG ONLY
+void printTree(Tree* t);
